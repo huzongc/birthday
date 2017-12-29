5d68dc52bad40ac0a866fc66c5c0c6be19482cc1
huzongcheng <huzongcheng@leadu.net.cn>
2017-12-29 14:48:13 +0800

    modify

--- yourbirthday/js/functions.js
+++ yourbirthday/js/functions.js
@@ -3,9 +3,25 @@ var $window = $(window), gardenCtx, gardenCanvas, $garden, garden;
 var clientWidth = $(window).width();
 var clientHeight = $(window).height();
 
-$(function(){
-	$("#audio")[0].play();
-})
+//--创建页面监听，等待微信端页面加载完毕 触发音频播放
+document.addEventListener('DOMContentLoaded', function () {
+	function audioAutoPlay() {
+		var audio = document.getElementById('audio');
+		audio.play();
+		document.addEventListener("WeixinJSBridgeReady", function () {
+			audio.play();
+		}, false);
+	}
+	audioAutoPlay();
+});
+//--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
+document.addEventListener('touchstart', function () {
+	function audioAutoPlay() {
+		var audio = document.getElementById('audio');
+		audio.play();
+	}
+	audioAutoPlay();
+});
 
 $(function () {
     // setup garden
